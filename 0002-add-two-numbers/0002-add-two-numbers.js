26848d231776ba19888bc6e3c772e9c33c6ebea8
akhileshGarg80 <aynex963010@gmail.com>
2026-08-08 22:10:10 +0530

    Time: 2 ms (85.73%), Space: 61.4 MB (66.5%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.js
+++ 0002-add-two-numbers/0002-add-two-numbers.js
@@ -1,25 +1,16 @@
 var addTwoNumbers = function(l1, l2) {
-    let dummy = new ListNode(0);
-    let current = dummy;
+    const head = new ListNode(0);
+    let curr = head;
     let carry = 0;
     
-    while (l1 !== null || l2 !== null || carry > 0) {
-        let sum = carry;
-        
-        if (l1 !== null) {
-            sum += l1.val;
-            l1 = l1.next;
-        }
-        
-        if (l2 !== null) {
-            sum += l2.val;
-            l2 = l2.next;
-        }
-        
-        carry = Math.floor(sum / 10);
-        current.next = new ListNode(sum % 10);
-        current = current.next;
+    while (l1 || l2 || carry) {
+        const sum = (l1?.val || 0) + (l2?.val || 0) + carry;
+        carry = sum > 9 ? 1 : 0;
+        curr.next = new ListNode(sum % 10);
+        curr = curr.next;
+        l1 = l1?.next || null;
+        l2 = l2?.next || null;
     }
     
-    return dummy.next;
+    return head.next;
 };
