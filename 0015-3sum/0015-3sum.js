3d178541e55777accf3aafb91e792eb033589da7
akhileshGarg80 <aynex963010@gmail.com>
2026-08-14 15:21:08 +0530

    Time: 31 ms (87.78%), Space: 72.8 MB (37.54%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -1,57 +1,29 @@
-var threeSum = function(nums)
-{
+var threeSum = function(nums) {
+    nums.sort((a, b) => a - b);
+    let ans = [];
 
+    for (let i = 0; i < nums.length - 2; i++) {
+        if (i && nums[i] === nums[i - 1]) continue;
+        if (nums[i] > 0) break;
 
+        let l = i + 1, r = nums.length - 1;
 
- nums.sort((a,b)=> a-b);
+        while (l < r) {
+            let sum = nums[i] + nums[l] + nums[r];
 
- const result =[];
- 
- const n = nums.length ;
+            if (sum === 0) {
+                ans.push([nums[i], nums[l], nums[r]]);
 
+                while (l < r && nums[l] === nums[l + 1]) l++;
+                while (l < r && nums[r] === nums[r - 1]) r--;
 
- for (let i = 0 ; i < n -2; i++) 
- {
-    if(i>0 && nums[i]===nums[i-1])
-    continue;
-
-    if(nums[i]>0) break;
-
-    let left = i +1;
-    let right = n-1;
-
-
-    while(left<right){
-        const sum = nums[i] + nums [left] + nums [right];
-
-
-        if (sum===0){
-            result.push([nums[i],nums[left],nums[right]]);
-
-            //skip to a duplicaton 
-
-             while ( left<right && nums[left]===nums[left+1])left++;
-
-             while (left<right && nums[right]===nums[right-1])right--;
-
-
-             left++;
-             right--;
-
-        }
-
-        else if (sum<0){
-            left++;
-
-        }
-
-
-        else{
-            right--;
+                l++;
+                r--;
+            } 
+            else if (sum < 0) l++;
+            else r--;
         }
     }
 
- }
-
- return result;
-}
+    return ans;
+};
