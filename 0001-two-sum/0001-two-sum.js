05f1b171c0273b6238c07e08d0f96f0417293567
akhileshGarg80 <aynex963010@gmail.com>
2026-09-26 00:45:32 +0530

    Update 0001-two-sum.js
    All merhod

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -1,10 +1,16 @@
-var twoSum = function(nums, target) {
-    const map = {};
-    for (let i = 0; i < nums.length; i++) {
-        const complement = target - nums[i];
-        if (map[complement] !== undefined) {
-            return [map[complement], i];
-        }
-        map[nums[i]] = i;
+/**
+ * O(n) time, O(MAX) space (bit‑set)
+ */
+function twoSumBitSet(nums, target, MAX = 1_000_000) {
+  const seen = new Uint8Array(MAX + 1); // 0/1 flags
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    if (complement >= 0 && complement <= MAX && seen[complement]) {
+      // Need a second pass to fetch the stored index.
+      const j = nums.findIndex((v, idx) => idx !== i && v === complement);
+      return [j, i];
     }
-};
+    if (nums[i] >= 0 && nums[i] <= MAX) seen[nums[i]] = 1;
+  }
+  return [];
+}
